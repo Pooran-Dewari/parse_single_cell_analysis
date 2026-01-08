68c68cbe31b543b7aacb8b70c792affbbecd0f83
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2026-01-08 10:33:58 +0000

    Add files via upload

---
+++ 08_SRA_submission/split_fastq/qsub_split_A2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#$ -V -cwd
+#$ -l h_rt=08:00:00
+#$ -l h_vmem=40G
+#$ -pe sharedmem 1
+
+
+module load roslin/python/3.8.10
+
+SCRIPTPATH="/exports/eddie/scratch/pdewari/concat/fastq_sep_groups_new2024.py" 
+
+FQ_DIR="/exports/eddie/scratch/pdewari/concat/"
+
+python $SCRIPTPATH \
+--chemistry v2 \
+--fq1 ${FQ_DIR}A2_EKDL240002474-1A_223M7CLT4_1.fq.gz \
+--fq2 ${FQ_DIR}A2_EKDL240002474-1A_223M7CLT4_2.fq.gz \
+--opath ${FQ_DIR}split-fq \
+--group shrimp A1-C8 \
+--group oyster C9-D12
